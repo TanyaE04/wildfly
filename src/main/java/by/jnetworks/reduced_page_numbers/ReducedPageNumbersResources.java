862aca2cbecd3e662838e8d9275152fbc75a9de9
tanyalivanovich <tanya1potapova@gmail.com>
2020-01-07 21:52:52 +0300

    resources

--- src/main/java/by/jnetworks/reduced_page_numbers/ReducedPageNumbersResources.java
+++ src/main/java/by/jnetworks/reduced_page_numbers/ReducedPageNumbersResources.java
@@ -11,14 +11,13 @@ public class ReducedPageNumbersResources {
     @GET
     @Produces (MediaType.APPLICATION_JSON)
     public String getPageNumbers (@QueryParam ("rawPageNumbers") String str) {
-
-        String res;
+        String result;
         try {
-            res = "{\n\t\"original\": "+str+"\n\t\"reduced\": "+Util.reducedPageNumbers (str)+"\n}";
+            result = "{\n\t\"original\": "+str+"\n\t\"reduced\": "+Util.reducedPageNumbers (str)+"\n}";
         } catch (NumberFormatException e){
             Response.status (500).build ();
-            res = e.toString ();
+            result = e.toString ();
         }
-        return res;
+        return result;
     }
 }
